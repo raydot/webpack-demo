0b83100c31a0d8f6d569b37b3ce31805c22d81d1
David Kanter <davidkanter@sf-aqt-dkanter.macromedia.com>
2018-08-03 11:21:54 -0700

    Finished the basic tutorial.  A lot of things going on in Webpack.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import _ from 'lodash';
+import _ from 'lodash';  //import lodash
+import './style.css'; //import style.css
+import Icon from './icon.png'; //this can't be the best way to manage images
+import Data from './data.xml'; //load xml data
 
 function component() {
 	var element = document.createElement('div');
@@ -6,6 +9,15 @@ function component() {
 	// Lodash, currently included via a script, is required for this line to work
 	// NOPE!  Lodash is now imported by this script
 	element.innerHTML = _.join(['Hello', 'from', 'webpack'], ' ');
+	element.classList.add('hello');
+
+	// Add the image to our existing div.
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
+	console.log(Data);
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,40 @@ module.exports = {
 	output: {
 		filename: 'main.js', 
 		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader', //loads string into style tags		#2
+					'css-loader', //generates a string from all css  	#1
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(csv|tsv)$/,
+				use: [
+					'csv-loader'
+				]
+			},
+			{
+				test: /\.xml$/,
+				use: [
+					'xml-loader'
+				]
+			}
+		]
 	}
 };
